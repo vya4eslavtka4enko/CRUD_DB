739f0cab7198dbf8df45428a33c9c8e2385d2886
Viacheslav <vya4eslav.tka4enko@gmail.com>
2023-11-24 22:29:22 +0000

    t

--- main.py
+++ main.py
@@ -1,29 +1,53 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask,render_template,request,redirect,url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///books-collection.db'
+db = SQLAlchemy()
+db.init_app(app)
+# all_books = []
 
-all_books = []
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
 
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def home():
-    return render_template("index.html", books=all_books)
+        result = db.session.execute(db.select(Book).order_by(Book.title))
+        all_books = result.scalars()
+        return render_template("index.html", books=all_books)
 
+
+@app.route("/edit", methods=["GET", "POST"])
+def edit():
+    if request.method == "POST":
+        #UPDATE RECORD
+        book_id = request.form["id"]
+        book_to_update = db.get_or_404(Book, book_id)
+        book_to_update.rating = request.form["rating"]
+        db.session.commit()
+        return redirect(url_for('home'))
+    book_id = request.args.get('id')
+    book_selected = db.get_or_404(Book, book_id)
+    return render_template("edit_rating.html", book=book_selected)
 
 @app.route("/add", methods=["GET", "POST"])
 def add():
     if request.method == "POST":
-        new_book = {
-            "title": request.form["title"],
-            "author": request.form["author"],
-            "rating": request.form["rating"]
-        }
-        all_books.append(new_book)
-
-        # NOTE: You can use the redirect method from flask to redirect to another route
-        # e.g. in this case to the home page after the form has been submitted.
+        # CREATE RECORD
+        new_book = Book(
+            title=request.form["title"],
+            author=request.form["author"],
+            rating=request.form["rating"]
+        )
+        db.session.add(new_book)
+        db.session.commit()
         return redirect(url_for('home'))
-
     return render_template("add.html")
 
 
